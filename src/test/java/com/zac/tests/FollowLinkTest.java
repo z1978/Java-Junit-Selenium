58858dc8ddd0091ad9f192b870c2191839c4cb54
zac <zgithub@hotmail.com>
2017-10-27 01:04:05 +0900

    first commit

---
+++ src/test/java/com/zac/tests/FollowLinkTest.java
@@ -0,0 +1,29 @@
+package com.zac.tests;
+
+import com.zac.pages.*;
+
+import org.junit.Test;
+import org.openqa.selenium.InvalidElementStateException;
+
+import static org.junit.Assert.*;
+
+public class FollowLinkTest extends TestBase {
+
+    public FollowLinkTest(String os,
+                          String version, String browser, String deviceName, String deviceOrientation) {
+            super(os, version, browser, deviceName, deviceOrientation);
+    }
+
+    /**
+     * Runs a simple test verifying link can be followed.
+     * @throws InvalidElementStateException
+     */
+    @Test
+    public void verifyLinkTest() throws InvalidElementStateException {
+        GuineaPigPage page = GuineaPigPage.visitPage(driver);
+
+        page.followLink();
+
+        assertFalse(page.isOnPage());
+    }
+}
